9f1142a3e3da6b67d851f54aa6bb10ef7325f2e2
Uditha Jayalath <udithajayalath4@gmail.com>
2024-05-25 23:38:29 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Awesome blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/about" element={<AboutPage/>} />
+            <Route path="/articles" element={<ArticlesListPage/>} />
+            <Route path="/articles/:articleId" element={<ArticlePage/>} />
+          </Routes>          
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+const ArticlesListPage = () => {
+    return (
+        <h1>This is the Articles List page!</h1>
+    )
+}
+
+export default ArticlesListPage;
