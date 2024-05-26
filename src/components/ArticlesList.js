13313ec1cef399b68414e2a181132c48f9064720
Uditha Jayalath <udithajayalath4@gmail.com>
2024-05-26 13:32:54 +0530

    fourth commit

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+const ArticlesList = ({articles}) => {
+    return(
+        <>
+        {articles.map(article => (
+            
+            // eslint-disable-next-line no-template-curly-in-string
+            <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0, 150)}</p>
+            </Link>
+        ))}
+        </>
+    )
+}
+
+export default ArticlesList;
